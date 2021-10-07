c102e98888b904e660ff3cf6ebb3752ccffcbaf1
hiratyan <monokeshi5121@gmail.com>
2021-10-07 18:13:53 +0900

    fix

--- test_dir/open_test/sample3.py
+++ test_dir/open_test/sample3.py
@@ -6,14 +6,14 @@ cap = cv2.VideoCapture(0)
 # 無限にループする
 while True:
 # カメラから画像を読み取り,img に格納する
-ret, img = cap.read()
+    ret, img = cap.read()
 # 画像を表示する
-cv2.imshow(’image’, img)
+    cv2.imshow('image', img)
 # キー入力を待つ
-c = cv2.waitKey(1000)
+    c = cv2.waitKey(1000)
 # ESC キーが押されたら終了する
-if c == 27:
-break;
+    if c == 27:
+        break;
 # デバイスを解放する
 cap.release()
 # 画面表示を消去
