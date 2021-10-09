f6906c269a22a755934ce1675327a8ead5d39cf7
hiratyan <monokeshi5121@gmail.com>
2021-10-10 00:14:40 +0900

    edit

--- test_dir/open_test/open_test_import/smooth2.py
+++ test_dir/open_test/open_test_import/smooth2.py
@@ -4,10 +4,20 @@ import cv2
 import sys
 
 def smooth(i):
+    blur_img = cv2.blur(i, (300,300) )
+    cv2.imshow('Original image', blur_img) 
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows() 
+
+def test(i):
     img2 = cv2.imread(i, cv2.IMREAD_COLOR )
-    blur_img2 = cv2.blur(img2, (30,30) )
+    blur_img2 = cv2.blur(i, (30,30) )
     cv2.imshow('Original image', img2) # 画像表示
     cv2.imshow('Blurred image', blur_img2) # 画像表示
     cv2.waitKey(0) # キー入力を待つ
     cv2.destroyAllWindows() # 画面表示を消去
+
+if __name__ == "__main__":
+    test(sys.argv[1])
+
 
