d89799c4020e5987696090584860ba41803bb446
hiratyan <monokeshi5121@gmail.com>
2021-12-15 21:20:52 +0900

    motorOK

--- test_dir/gomi_test/motor.py
+++ test_dir/gomi_test/motor.py
@@ -1,16 +1,35 @@
 import RPi.GPIO as GPIO
 import time
 
-in1_pin = 27
-in2_pin = 22
+Ain1 = 26
+Ain2 = 22
+Bin1 = 27
+Bin2 = 17
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1_pin, GPIO.OUT)
-GPIO.setup(in2_pin, GPIO.OUT)
+GPIO.setup(Ain1, GPIO.OUT)
+GPIO.setup(Ain2, GPIO.OUT)
+GPIO.setup(Bin1, GPIO.OUT)
+GPIO.setup(Bin2, GPIO.OUT)
 
 for i in range(5):
-    GPIO.output(in1_pin, True)
+    GPIO.output(Ain1, True)
     time.sleep(3)
-    GPIO.output(in1_pin, False)
+    GPIO.output(Ain1, False)
     time.sleep(3)
+    GPIO.output(Ain2, True)
+    time.sleep(3)
+    GPIO.output(Ain2, False)
+    time.sleep(3)
+
+    GPIO.output(Bin1, True)
+    time.sleep(3)
+    GPIO.output(Bin1, False)
+    time.sleep(3)
+    GPIO.output(Bin2, True)
+    time.sleep(3)
+    GPIO.output(Bin2, False)
+    time.sleep(3)
+
 GPIO.cleanup()
